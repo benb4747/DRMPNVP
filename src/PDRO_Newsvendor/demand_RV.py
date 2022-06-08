e07cfcb425a83a5cde213e38da76b89173bfdfaa
benb4747 <b.black1@lancaster.ac.uk>
2022-06-08 12:54:11 +0100

    Add Poisson MLEs to demand_RV

--- src/PDRO_Newsvendor/demand_RV.py
+++ src/PDRO_Newsvendor/demand_RV.py
@@ -12,18 +12,22 @@ class demand_RV:
         self.N = N
 
     def compute_mles(self):
-        if self.dist == "normal":
+        if self.dist == "normal" or self.dist == "Normal":
             mu, sig = self.omega_0
             Sig = np.zeros((self.T, self.T))
             for t in range(self.T):
-                Sig[t, t] = sig[t] ** 2
+                Sig[t, t] = sig[t]
             np.random.seed(self.seed)
             samples = np.random.multivariate_normal(mean=mu, cov=Sig, size=self.N)
             mu_MLE = [np.mean(samples[:, t]) for t in range(self.T)]
 
-            sig_MLE = [
-                np.sqrt(np.mean((samples[:, t] - mu_MLE[t]) ** 2))
-                for t in range(self.T)
-            ]
+            sig_MLE = [np.mean((samples[:, t] - mu_MLE[t])**2)
+                       for t in range(self.T)]
 
-            self.mle = (mu_MLE, sig_MLE)
+            self.mle = ((mu_MLE, sig_MLE))
+        elif self.dist == "poisson" or self.dist == "Poisson":
+            lam = self.omega_0
+            np.random.seed(self.seed)
+            samples = np.random.poisson(lam=lam, size=(self.N, self.T))
+            
+            self.mle = tuple([np.mean(samples[:, t]) for t in range(self.T)])
