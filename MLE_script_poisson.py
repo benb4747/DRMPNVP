695eb44fcf8e2a0742d1649fef45f28d1836f4ad
benb4747 <b.black1@lancaster.ac.uk>
2022-06-17 13:00:11 +0100

    Fix generation of lam0 and remake sh scripts in unix format

--- MLE_script_poisson.py
+++ MLE_script_poisson.py
@@ -204,8 +204,8 @@ for T_ in T_vals:
     lam_range = [lam_0_range for t in range(T_)]
     lam0_all = list(it.product(*lam_range))
     np.random.seed(T_vals.index(T_))
-    indices = np.random.choice(range(len(lam0_all[T_vals.index(T_)])), num_lam0)
-    lam0_vals.append([lam0_all[T_vals.index(T_)][i] for i in indices])
+    indices = np.random.choice(range(len(lam0_all)), num_lam0)
+    lam0_vals.append([lam0_all[i] for i in indices])
 
 inputs = [
     (
